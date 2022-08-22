e4df5573399adf210e42db8dc9aa26d9523bd5b0
Grave1ine <vladislav_barinov@bk.ru>
2022-08-22 22:40:45 +0300

    ввод данных и их вычисление по формулам

---
+++ DZ_2022_08_13_2/main.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+                                  //ввод данных и их вычисления
+int main()
+{
+
+  float a=5, c=5, b, d;
+
+
+
+  printf("input b=");
+  scanf("%f", &b);
+  printf("a=a+b-2\n");
+  a=a+b-2;
+  printf("a=%.2f\n",a);
+  printf("b=%.2f\n",b);
+
+  printf("-------------\n");
+
+  printf("c=%.2f\n",c);
+  c=c+1;
+  printf("c=c+1\n");
+  printf("c=%.2f\n",c);
+
+  printf("-------------\n");
+
+  printf("input d=");
+  scanf("%f", &d);
+  d=c-a+d;
+  printf("d=c-a+d\n");
+  printf("d=%.2f\n",d);
+
+  printf("-------------\n");
+
+  printf("a=a*c\n");
+  a=a*c;
+  printf("a=%.2f\n",a);
+
+  printf("-------------\n");
+
+  printf("c=c-1\n");
+  c=c-1;
+  printf("c=%.2f\n", c);
+
+  printf("-------------\n");
+
+  printf("a=a/10\n");
+  a=a/10;
+  printf("a=%.2f\n",a);
+
+  printf("-------------\n");
+
+  printf("c=c/2\n");
+  c=c/2;
+  printf("c=%.2f\n",c);
+
+  printf("-------------\n");
+
+  printf("b=b-1\n");
+  b=b-1;
+  printf("b=%.2f\n",b);
+
+  printf("-------------\n");
+
+  printf("d=d*(c+b+a)\n");
+  d=d*(c+b+a);
+  printf("d=%.2f\n",d);
+
+
+  return 0;
+}
