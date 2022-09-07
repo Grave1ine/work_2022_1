c744c0bb5a1ec88251af3e41560902925f7b91c5
Grave1ine <vladislav_barinov@bk.ru>
2022-09-07 14:38:39 +0300

    2.0

--- DZ_2022_09_03_01/main.cpp
+++ DZ_2022_09_03_01/main.cpp
@@ -5,40 +5,39 @@
 int main()
 {
 int M[40];
-srand(time(nullptr));
+srand(time(nullptr));           //рандомим массив и заполняем его
 for(int i = 0; i < 40; ++i)
 {
-    M[i] = rand() % 100 -50;
+    M[i] = rand() % 100 -50;    //задается интервал верхний и нижний предел рандома
     printf("%3d\n", M[i]);
 }
 int i;
 int max = 0, maxx;
-
-for(i = 0; i < 40; ++ i)
+for(i = 0; i < 40; ++ i)        //находим max значение
 {
     if(M[i] >= max)
     {
         max = M[i];
-        maxx = i;
+        maxx = i;               //записываем адрес обнаруженного значения
     }
 }
 int min = 0, minn;
-for(i = 0; i < 40; ++ i)
+for(i = 0; i < 40; ++ i)        //находим последнее min значение (если поставить M[i] < min) найдет наименьшее из вссего массива
 {
     if(M[i] < 0)
     {
         min = M[i];
-        minn = i;
+        minn = i;               //записываем адрес обнаруженного значения
     }
 }
-int tranzit = M[maxx];
+int tranzit = M[maxx];          //меняем их местами
 M[maxx] = M[minn];
 M[minn] = tranzit;
 printf("\n");
 printf("max value %d\n", max);
 printf("last min value %d\n", min);
 printf("\n");
-for(i = 0; i < 40; i ++)
+for(i = 0; i < 40; i ++)        //вывод результата
 {
     printf("%3d\n", M[i]);
 }
